a7540fa0a6916ed11407acdac0db0e9d416ac010
JosMuse <no-reply>
2020-11-21 12:03:51 -0700

    added readme

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// We need to provide an array of all urls that our PWA should cache. In other words, we're telling the PWA to be prepared to use the service worker anytime the browser tries to hit any of these routes. In a large web app there could be lots of entries here.
+
 const urlsToCache = [
   "/",
   "/db.js",
@@ -14,18 +14,16 @@ const urlsToCache = [
 ];
 
 
-// This is the heart of the PWA functionality. This code tells the service worker to listen for any events where a fetch (api call) is being made. This is when, normally, the browser would send a request to the server. You can use all of this code below as-is
+
 self.addEventListener("fetch", function(event) {
-  // By making sure all our fetch routes have the "/api/" prefix, it's easy to identify the ones we want to intercept
+ 
   if (event.request.url.includes("/api/")) {
     event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
 
-        // First we attempt to perform the fetch normally. In other words, if there is still an Internet connection, everything should just work normally.
         return fetch(event.request)
           .then(response => {
-            // If the response was good, we will store in the cache the name of the route that was accessed, and the data that was sent back.
-            // That way, if the same route is accessed later without an Internet connection, we can substitute the saved data.
+         
             if (response.status === 200) {
               cache.put(event.request.url, response.clone());
             }
@@ -33,9 +31,9 @@ self.addEventListener("fetch", function(event) {
             return response;
           })
 
-          // This code runs if the fetch fails; ie: there is no Internet connection. In this case it pulls the correct saved data from the cache and sends it back instead.
+       
           .catch(err => {
-            // Network request failed, try to get it from the cache.
+            
             return cache.match(event.request);
           });
       }).catch(err => console.log(err))
@@ -44,14 +42,14 @@ self.addEventListener("fetch", function(event) {
     return;
   }
 
-  // This code block handles all home page calls. Again, it can be used as-is.
+ 
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request).then(function(response) {
         if (response) {
           return response;
         } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
+          
           return caches.match("/");
         }
       });
